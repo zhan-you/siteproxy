6d67b1f6ccbe203585420387f13a4bc5aea84a77
zhan-you <67215289+zhan-you@users.noreply.github.com>
2022-03-30 19:22:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.workers.dev',
+    serverName: 'nextjs-seven-roan-25.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
